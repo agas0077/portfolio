828e90304acfdf6bbc0aa73ec29241d315e94d4e
Andrey Dell Latitude <at-am@yandex.ru>
2023-10-15 00:20:47 +0300

    change csrf trusted origins

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -34,10 +34,7 @@ ADMINS = ("Andrey", "at-am@yandex.ru")
 
 # CSRF_COOKIE_SECURE = True
 CSRF_TRUSTED_ORIGINS = [
-    'https://andrey-agas-portfolio.website',
-    'http://andrey-agas-portfolio.website',
     'http://portfolio:8000/', 
-    'http://localhost:8001',
 ]
 # Application definition
 
