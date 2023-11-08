5d5b35808d600bae178c91ced4f1b7d2426218ac
Andrey Dell Latitude <at-am@yandex.ru>
2023-11-09 00:25:07 +0300

    Add load more button to both projects and other projects

--- portfolio/static/assets/js/load_more_button.js
+++ portfolio/static/assets/js/load_more_button.js
@@ -1,25 +1,30 @@
 const projectNumIncrement = 2;
 
-function getHiddenProjects() {
-  return [].slice.call(document.querySelectorAll(".project-card[hidden]"));
+function getHiddenProjects(buttonElement) {
+  const cardsDiv = buttonElement.parentElement.parentElement;
+  return [].slice.call(cardsDiv.querySelectorAll(".project-card[hidden]"));
 }
 
-function displayHidden() {
-  projectToDisplay = getHiddenProjects().slice(0, projectNumIncrement);
+function displayHidden(button) {
+  projectToDisplay = getHiddenProjects(button).slice(0, projectNumIncrement);
 
   projectToDisplay.forEach((element) => {
     element.removeAttribute("hidden");
   });
 }
 
-const loadMoreButton = document.getElementById("load-more-projects-button");
-loadMoreButton.addEventListener("click", (event) => {
-  event.preventDefault();
-  displayHidden();
+const loadMoreButtons = [].slice.call(
+  document.getElementsByClassName("load-more-projects-button")
+);
+loadMoreButtons.forEach((button) => {
+  displayHidden(button);
+  button.addEventListener("click", (event) => {
+    event.preventDefault();
+    displayHidden(button);
 
-  if (getHiddenProjects().length === 0) {
-    loadMoreButton.setAttribute("disabled", "");
-  }
+    if (getHiddenProjects(button).length === 0) {
+      button.setAttribute("disabled", "");
+    }
+  });
 });
 
-displayHidden();
