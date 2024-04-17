567df5fa24d5067d003ca059cc32ef2efb791369
Andrey Agasiants <61889096+agas0077@users.noreply.github.com>
2024-04-17 13:01:04 +0300

    Update settings.py

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ADMINS = ("Andrey", "at-am@yandex.ru")
+ADMINS = [("Andrey", "at-am@yandex.ru"),]
 
 ALLOWED_HOSTS = [
     "localhost",
